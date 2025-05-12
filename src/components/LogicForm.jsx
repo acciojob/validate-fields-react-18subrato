09d918e3995343001fd5d93f9f16c1db09fa75ae
18subrato <subratoverma7@gmail.com>
2025-05-12 17:30:46 +0530

    initial commit

---
+++ src/components/LogicForm.jsx
@@ -0,0 +1,35 @@
+import React, { useState } from 'react'
+
+const LogicForm = () => {
+    const[username,setUsername] = useState('');
+    const[password,setPassword] = useState('')
+    const[error,setError] = useState(false);
+    function handleSubmit(e){
+        e.preventDefault();
+        if(!username){
+
+           setError('username is required');
+           
+            return;
+        }
+
+        if(!password){
+           setError('password is required')
+            return;
+        }
+        setUsername('');
+        setPassword('');
+    }
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input id='username' value={username} onChange={(e)=>setUsername(e.target.value)} />
+        <input id='password' value={password} onChange={(e)=>setPassword(e.target.value)} />
+        <button type='submit'>Submit</button>
+      </form>
+      {error ? <div id='errorMessage' style={{color:'red'}}>{error}</div>:''}
+    </div>
+  )
+}
+
+export default LogicForm
